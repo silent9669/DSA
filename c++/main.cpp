3417d9cc1dd93210d392cc2e8a3d3e89c1f0e21e
silent9669 <silent9669@users.noreply.github.com>
2026-05-21 20:08:45 +0700

    Initial Commit

---
+++ c++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by phucdang on 21/5/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
